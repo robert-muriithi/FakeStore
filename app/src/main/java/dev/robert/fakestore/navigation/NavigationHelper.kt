6b02631d5e6dabdc5cd8d107bb74de4fda5fdd92
Ericgacoki <gacokieric@gmail.com>
2024-01-27 13:46:02 +0300

    indicate current screen
    add .idea to git ignore

--- app/src/main/java/dev/robert/fakestore/navigation/NavigationHelper.kt
+++ app/src/main/java/dev/robert/fakestore/navigation/NavigationHelper.kt
@@ -5,12 +5,10 @@ import com.ramcosta.composedestinations.dynamic.within
 import com.ramcosta.composedestinations.navigation.navigate
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import dev.robert.cart.presentation.CartScreenNavigator
-import dev.robert.cart.presentation.destinations.CheckoutScreenDestination
 import dev.robert.products.domain.model.Product
 import dev.robert.products.presentation.HomeScreenNavigator
 import dev.robert.products.presentation.ProductDetailsScreenNavigator
 import dev.robert.products.presentation.destinations.HomeScreenDestination
-import dev.robert.products.presentation.destinations.ProductDetailsScreenDestination
 import dev.robert.user.presentation.ProfileScreenNavigator
 
 class NavigationHelper(
