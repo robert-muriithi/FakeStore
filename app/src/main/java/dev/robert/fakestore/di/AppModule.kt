b10c9638d5de329f448060004f2118a846ac1866
robert-muriithi <njuguiinirobert@gmail.com>
2024-02-02 11:42:36 +0300

    store theme preference on datastore

---
+++ app/src/main/java/dev/robert/fakestore/di/AppModule.kt
@@ -0,0 +1,28 @@
+package dev.robert.fakestore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideDatastorePreferences(@ApplicationContext context: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                context.preferencesDataStoreFile(FAKE_STORE_PREFS)
+            }
+        )
+
+    private const val FAKE_STORE_PREFS = "FAKE_STORE_PREFS"
+}
